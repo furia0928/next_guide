2cd1956952c40e595a0966648115fd0d01e892ca
jay <furia0928@gmail.com>
2025-01-08 16:44:35 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "local") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
 };
 
 export default nextConfig;
