c02a87248681d95ae8746cf88cc6546bcf329999
jay <furia0928@gmail.com>
2025-01-15 11:37:58 +0900

    init

--- src/components/layout/Wrap.js
+++ src/components/layout/Wrap.js
@@ -1,12 +1,7 @@
 import Header from '@/components/layout/Header'
 
 function Wrap({ children }) {
-  return (
-    <div id="wrap">
-      <>{children}</>
-      <footer>기본 푸터</footer>
-    </div>
-  )
+  return <div id="wrap">{children}</div>
 }
 
 export default Wrap
